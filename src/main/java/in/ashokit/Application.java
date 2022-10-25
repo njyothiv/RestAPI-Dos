4dfe639bb83aade67ad06af853dea01d1ed35f3a
Jyothi <njyothiv22@gmail.com>
2022-10-25 17:29:22 -0400

    updated with loadcaching logic

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -3,13 +3,17 @@ package in.ashokit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import java.util.*;
 
 @SpringBootApplication
 @EnableCaching
 public class Application {
-
+	private Map<String,Object> cache = new HashMap<String,Object>();
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	public void loadDataToCache() {
+			//logic
 	}
 
 }
