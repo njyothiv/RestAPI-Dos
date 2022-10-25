c35944fdf78d60a8307368091a106c9e39fb6ed7
Jyothi <njyothiv22@gmail.com>
2022-10-25 17:19:53 -0400

    add Rest API call for feature-300

--- src/main/java/in/ashokit/Application.java
+++ src/main/java/in/ashokit/Application.java
@@ -7,9 +7,11 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 public class Application {
-
+	private static final String REST_URL = "http://www.equifax.com/gteScores";
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
 
 }
